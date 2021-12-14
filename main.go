45f6b43c37749d2fa368f6006e0d5cb2d1a4bcb5
kooooohe <1211lta+github@gmail.com>
2021-12-15 01:09:11 +0900

    divied to func making CandleSticks

--- main.go
+++ main.go
@@ -8,9 +8,12 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strconv"
+	"time"
 )
 
 type CandleStick struct {
+	date  time.Time
 	start int
 	high  int
 	low   int
@@ -42,39 +45,67 @@ func targetFiles(tDir string) (tFiles []string, err error) {
 		return nil
 	})
 	if err != nil {
-		return  nil, err
+		return nil, err
 	}
-	return 
+	return
+}
+
+func CandleSticks(paths []string) ([]CandleStick, error) {
+	rc := []CandleStick{}
+	for _, v := range paths {
+		f, err := os.Open(v)
+		if err != nil {
+			return nil, err
+		}
+		r := csv.NewReader(f)
+		cnt := 2
+		r.FieldsPerRecord = -1
+
+		for {
+			recode, err := r.Read()
+			if cnt > 0 {
+				cnt--
+				continue
+			}
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				return nil, err
+			}
+			// 日付	始値	高値	安値	終値	出来高	終値調整値
+			s, _ := strconv.Atoi(recode[1])
+			h, _ := strconv.Atoi(recode[2])
+			l, _ := strconv.Atoi(recode[3])
+			e, _ := strconv.Atoi(recode[4])
+			c := CandleStick{
+				// TODO date
+				start: s,
+				high:  h,
+				low:   l,
+				end:   e,
+			}
+			fmt.Printf("%T: %v\n", recode, recode)
+			rc = append(rc, c)
+		}
+
+		f.Close()
+	}
+	return rc, nil
 }
 
 func main() {
-	// tDir := "./data/6103"
-	ns, err:= targetFiles("./data/6103")
+	ns, err := targetFiles("./data/6103")
+	if err != nil {
+		log.Fatalln(err)
+	}
 	fmt.Println(ns)
-	return
-
-	f, err := os.Open("./data/6103/6103_2015.csv")
+	vs, err := CandleSticks(ns)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	r := csv.NewReader(f)
-	cnt := 2
-	r.FieldsPerRecord = -1
-
-	for {
-		recode, err := r.Read()
-		if cnt > 0 {
-			cnt--
-			continue
-		}
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("%T: %v\n", recode, recode)
+	// fmt.Println(vs)
+	for _, v := range vs {
+		fmt.Println(v)
 	}
-	// println("test")
 }

47700aaa2a61a2cbe7e578e98a637731f19f68c6
kooooohe <1211lta+github@gmail.com>
2021-12-16 00:17:17 +0900

    divided a file

--- main.go
+++ main.go
@@ -1,36 +1,12 @@
 package main
 
 import (
-	"encoding/csv"
 	"fmt"
-	"io"
 	"io/fs"
 	"log"
-	"os"
 	"path/filepath"
-	"strconv"
-	"strings"
-	"time"
+	"system-trade/system-trade"
 )
-
-type CandleStick struct {
-	date  time.Time
-	start int
-	high  int
-	low   int
-	end   int
-}
-
-type DMA struct {
-	candleSticks []CandleStick
-}
-
-func (d DMA) CurrentAvarage(du int) int {
-	return 0
-}
-func (d DMA) CurrentAvarageWithin(du int, b int) int {
-	return 0
-}
 
 func targetFiles(tDir string) (tFiles []string, err error) {
 	err = filepath.Walk(tDir, func(path string, info fs.FileInfo, err error) error {
@@ -51,65 +27,18 @@ func targetFiles(tDir string) (tFiles []string, err error) {
 	return
 }
 
-func CandleSticks(paths []string) ([]CandleStick, error) {
-	rc := []CandleStick{}
-	for _, v := range paths {
-		f, err := os.Open(v)
-		if err != nil {
-			return nil, err
-		}
-		r := csv.NewReader(f)
-		cnt := 2
-		r.FieldsPerRecord = -1
-
-		for {
-			recode, err := r.Read()
-			if cnt > 0 {
-				cnt--
-				continue
-			}
-			if err == io.EOF {
-				break
-			}
-			if err != nil {
-				return nil, err
-			}
-			// 日付,始値,高値,安値,終値,出来高,終値,調整値
-			d := strings.Replace(recode[0],"-","/",-1)
-			layout := "2006/01/02"
-			dt, _ :=  time.Parse(layout, d)
-			s, _ := strconv.Atoi(recode[1])
-			h, _ := strconv.Atoi(recode[2])
-			l, _ := strconv.Atoi(recode[3])
-			e, _ := strconv.Atoi(recode[4])
-			c := CandleStick{
-				date: dt,
-				start: s,
-				high:  h,
-				low:   l,
-				end:   e,
-			}
-			// fmt.Printf("%T: %v\n", recode, recode)
-			rc = append(rc, c)
-		}
-
-		f.Close()
-	}
-	return rc, nil
-}
-
 func main() {
 	ns, err := targetFiles("./data/6103")
 	if err != nil {
 		log.Fatalln(err)
 	}
 	fmt.Println(ns)
-	vs, err := CandleSticks(ns)
+	vs, err := systemtrade.CandleSticks(ns)
 	if err != nil {
 		log.Fatal(err)
 	}
 	// fmt.Println(vs)
 	for _, v := range vs {
-		fmt.Println(v.date)
+		fmt.Println(v.Date())
 	}
 }
---
+++ system-trade/stock.go
@@ -0,0 +1,80 @@
+package systemtrade
+
+import (
+	"encoding/csv"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type CandleStick struct {
+	date  time.Time
+	start int
+	high  int
+	low   int
+	end   int
+}
+
+func (c CandleStick) Date() time.Time {
+	return c.date
+}
+
+type DMA struct {
+	candleSticks []CandleStick
+}
+
+func (d DMA) CurrentAvarage(du int) int {
+	return 0
+}
+func (d DMA) CurrentAvarageWithin(du int, b int) int {
+	return 0
+}
+
+func CandleSticks(paths []string) ([]CandleStick, error) {
+	rc := []CandleStick{}
+	for _, v := range paths {
+		f, err := os.Open(v)
+		if err != nil {
+			return nil, err
+		}
+		r := csv.NewReader(f)
+		cnt := 2
+		r.FieldsPerRecord = -1
+
+		for {
+			recode, err := r.Read()
+			if cnt > 0 {
+				cnt--
+				continue
+			}
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				return nil, err
+			}
+			// 日付,始値,高値,安値,終値,出来高,終値,調整値
+			d := strings.Replace(recode[0], "-", "/", -1)
+			layout := "2006/01/02"
+			dt, _ := time.Parse(layout, d)
+			s, _ := strconv.Atoi(recode[1])
+			h, _ := strconv.Atoi(recode[2])
+			l, _ := strconv.Atoi(recode[3])
+			e, _ := strconv.Atoi(recode[4])
+			c := CandleStick{
+				date:  dt,
+				start: s,
+				high:  h,
+				low:   l,
+				end:   e,
+			}
+			// fmt.Printf("%T: %v\n", recode, recode)
+			rc = append(rc, c)
+		}
+
+		f.Close()
+	}
+	return rc, nil
+}

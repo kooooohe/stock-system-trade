0c72aad73c03bf6ae711acdbe112f6c28cacc0fe
kooooohe <1211lta+github@gmail.com>
2021-12-20 00:37:01 +0900

    on the way

--- main.go
+++ main.go
@@ -5,7 +5,8 @@ import (
 	"io/fs"
 	"log"
 	"path/filepath"
-	"system-trade/system-trade"
+	systemtrade "system-trade/system-trade"
+	"time"
 )
 
 func targetFiles(tDir string) (tFiles []string, err error) {
@@ -27,19 +28,77 @@ func targetFiles(tDir string) (tFiles []string, err error) {
 	return
 }
 
+func trade(sDate time.Time, cs systemtrade.CandleSticks) {
+	skipc := 10
+	p := 0
+
+	po := systemtrade.Position{Lc: 0.03, Lp: 0.07}
+	for i, v := range cs {
+		// for DMA
+		if skipc > 0 || v.Date.Before(sDate) {
+			skipc--
+			continue
+		}
+		d := cs.DMA(10, i)
+		fmt.Printf("%v: ", v.Date)
+		fmt.Println(d)
+		// fmt.Println(v.Date())
+		//TODO 10DMAが上向きで、株価のしたひげでも一回でもDMA以下にあって、次の日が高値を超えたら 3% 7%
+
+		wasDMAUp := cs.DMA(10, i-1) > cs.DMA(10, i-2)
+		wasStockUnderDMA := float64(v.Low) < cs.DMA(10, i-1)
+		wasStockOverDMA := float64(v.High) > cs.DMA(10, i-1)
+		yesterday := cs[i-1]
+
+		if po.IsBuying() {
+			if float64(v.Low) <= float64(po.Price()) * (1 -po.Lc) {
+			}
+			continue
+		}
+		if po.IsSelling() {
+
+			continue
+		}
+
+		if wasDMAUp && wasStockUnderDMA {
+			if v.High > yesterday.High {
+				p = yesterday.High + 1
+				if v.Start > yesterday.High {
+					p = v.Start
+				}
+				po.Buy(p)
+				fmt.Printf("Buy: %v: %v", v.Date, p)
+			}
+		}
+
+		if !wasDMAUp && wasStockOverDMA {
+			if v.Low < yesterday.Low {
+				p = yesterday.Low - 1
+				if v.Start < yesterday.Low {
+					p = v.Start
+				}
+				po.ShortSell(p)
+				fmt.Printf("ShortSell: %v: %v", v.Date, p)
+			}
+		}
+
+	}
+	// vs.DMA(10)
+}
+
 func main() {
 	ns, err := targetFiles("./data/6103")
 	if err != nil {
 		log.Fatalln(err)
 	}
-	fmt.Println(ns)
+	// fmt.Println(ns)
 	vs, err := systemtrade.MakeCandleSticks(ns)
 	if err != nil {
 		log.Fatal(err)
 	}
 	// fmt.Println(vs)
-	for _, v := range vs {
-		fmt.Println(v.Date())
-	}
-	vs.DMA(10)
+	// cnt := 0
+	layout := "2006/01/02"
+	sDate, _ := time.Parse(layout, "2015/01/01")
+	trade(sDate, vs)
 }
--- system-trade/stock.go
+++ system-trade/stock.go
@@ -3,33 +3,90 @@ package systemtrade
 import (
 	"encoding/csv"
 	"io"
+	"math"
 	"os"
+	"sort"
 	"strconv"
-	"strings"
+
+	// "strings"
 	"time"
 )
 
 type CandleStick struct {
-	date  time.Time
-	start int
-	high  int
-	low   int
-	end   int
-}
-
-func (c CandleStick) Date() time.Time {
-	return c.date
+	Date  time.Time
+	Start int
+	High  int
+	Low   int
+	End   int
 }
 
 type CandleSticks []CandleStick
 
-func (c CandleSticks) DMA(d int)(r float64) {
-	size := len(c) -1
-	for i:=0; i<d; i++ {
-		println(c[size-i].end)
-	}
-	return 
+func (c CandleSticks) sort() CandleSticks {
+	sort.Slice(c, func(i, j int) bool {
+		return c[i].Date.Before(c[j].Date)
+	})
+	return c
 }
+func (c CandleSticks) DMA(d int, to int) (r float64) {
+	sum := 0
+	// size := len(c) -1
+	for i := 0; i < d; i++ {
+		sum += c[to-i].End
+		// println(c[size-i].end)
+	}
+	return float64(sum) / float64(d)
+}
+
+type positionType int
+
+const (
+	nothing positionType = 0
+	buy     positionType = 1
+	sell    positionType = 2
+)
+
+type Position struct {
+	t     positionType
+	price int
+	Lc float64
+	Lp float64 //指値
+}
+
+func (po *Position)Buy(p int) {
+	po.price =p
+	po.t = buy
+}
+func (po *Position)ShortSell(p int) {
+	po.t = sell
+	po.price = p
+}
+
+func (po *Position) Sell(c CandleStick) int {
+	po.t = nothing
+	if float64(c.Start)<= float64(po.price)* (1 - po.Lc) {
+		return c.Start - po.price
+	}
+	return int(math.Ceil(float64(po.price) * po.Lc))
+	// TODO 利確のときも一緒にする？
+	// TODO 割合を出す？数値だけだと、具体的な期待値がでない
+}
+
+func (po *Position) BuyBack() {
+}
+func (po Position) IsDoing() bool {
+	return po.t != nothing
+}
+func (po Position) IsBuying() bool {
+	return po.t == buy
+}
+func (po Position) IsSelling() bool {
+	return po.t == sell
+}
+func (po Position) Price() int {
+	return po.price
+}
+
 
 
 type DMA struct {
@@ -51,7 +108,7 @@ func MakeCandleSticks(paths []string) (CandleSticks, error) {
 			return nil, err
 		}
 		r := csv.NewReader(f)
-		cnt := 2
+		cnt := 1
 		r.FieldsPerRecord = -1
 
 		for {
@@ -66,20 +123,21 @@ func MakeCandleSticks(paths []string) (CandleSticks, error) {
 			if err != nil {
 				return nil, err
 			}
-			// 日付,始値,高値,安値,終値,出来高,終値,調整値
-			d := strings.Replace(recode[0], "-", "/", -1)
-			layout := "2006/01/02"
-			dt, _ := time.Parse(layout, d)
+			// 日付,始値,高値,安値,終値,出来高,調整後終値
+			// d := strings.Replace(recode[0], "-", "/", -1)
+			layout := "2006/1/2"
+			// layout := "2006/01/02"
+			dt, _ := time.Parse(layout, recode[0])
 			s, _ := strconv.Atoi(recode[1])
 			h, _ := strconv.Atoi(recode[2])
 			l, _ := strconv.Atoi(recode[3])
 			e, _ := strconv.Atoi(recode[4])
 			c := CandleStick{
-				date:  dt,
-				start: s,
-				high:  h,
-				low:   l,
-				end:   e,
+				Date:  dt,
+				Start: s,
+				High:  h,
+				Low:   l,
+				End:   e,
 			}
 			// fmt.Printf("%T: %v\n", recode, recode)
 			rc = append(rc, c)
@@ -87,5 +145,5 @@ func MakeCandleSticks(paths []string) (CandleSticks, error) {
 
 		f.Close()
 	}
-	return rc, nil
+	return rc.sort(), nil
 }
